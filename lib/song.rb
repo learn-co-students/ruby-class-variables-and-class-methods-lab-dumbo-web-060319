9cc8382e43c3f505bb9402432717265b6e448824
Alexander Schelchere <ascellie@gmail.com>
2019-06-05 19:21:33 -0400

    artist count - gucci

--- lib/song.rb
+++ lib/song.rb
@@ -44,7 +44,17 @@ class Song
     end
 
     def self.artist_count
+        artist_hash = {}
 
+        @@artists.map do |artist|
+            if artist_hash.has_key? artist
+                artist_hash[artist] += 1
+            else
+                artist_hash[artist] = 1
+            end
+        end
+
+        artist_hash
     end
     
 
